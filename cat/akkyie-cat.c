434fbc2948de2b37687961a6d33694cc70944e79
Akkyie Y <akkyie01@gmail.com>
2015-04-23 23:03:11 +0900

    refactored

--- cat/akkyie-cat.c
+++ cat/akkyie-cat.c
@@ -24,35 +24,33 @@ void print(const char* str, size_t length, int fd) {
 }
 
 int main(int argc, const char* argv[]) {
-	switch (argc) {
-		case 0: {
-			ERROR("Too few arguments\n");
-			break;
-		}
-		default: {
-			bool stdi = argc == 1;
-			for (int i = 1; stdi || i < argc; i++) {
-				const char* path = argv[i];
-				int fd = stdi ? 0 : open(path, O_RDONLY);
 
-				if (fd == -1) {
-					ERROR("File read error\n");
-					return -1;
-				}
-
-				char buffer[BUFFER_SIZE + 1];
-				ssize_t rc = read(fd, buffer, BUFFER_SIZE);
-
-				if (rc == -1) {
-					ERROR("File read error\n");
-					return -1;
-				}
-
-				buffer[rc] = '\0';
-				PRINT(buffer);
-			}
-			break;
-		}
+	if (argc < 1) {
+		PRINT("Invalid invocation");
+		return -1;
 	}
+
+	bool stdi = argc == 1;
+	for (int i = 1; stdi || i < argc; i++) {
+		const char* path = argv[i];
+		int fd = stdi ? 0 : open(path, O_RDONLY);
+
+		if (fd == -1) {
+			ERROR("File read error\n");
+			return -1;
+		}
+
+		char buffer[BUFFER_SIZE + 1];
+		ssize_t rc = read(fd, buffer, BUFFER_SIZE);
+
+		if (rc == -1) {
+			ERROR("File read error\n");
+			return -1;
+		}
+
+		buffer[rc] = '\0';
+		PRINT(buffer);
+	}
+
 	return 0;
 }
