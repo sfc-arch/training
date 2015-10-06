afe750e78ee4d24f542a8005d28ee83a42cf36ea
makoto26 <makomori26@gmail.com>
2015-10-06 17:59:22 +0900

    Revised one.

--- cat.c
+++ cat.c
@@ -11,17 +11,17 @@ int main(int argc, const char *argv[]) {
 
   if (argc < 1) {
     write(2, "An error occured.\n", 18);
-  }else if (argc == 1) {
-    while((input = read(0, str, 1024)) > 0){
+  } else if (argc == 1) {
+    while ((input = read(0, str, 1024)) > 0) {
       write(1, str, input);
     }
-  }else{
-    for(int i = 1; i < argc; i++) {
+  } else {
+    for (int i = 1; i < argc; i++) {
       fd = open(argv[i],O_RDONLY);
 
       if (fd<0) {
         write(2, "An error occured.\n", 18);
-      }else{
+      } else {
         while ((length = read(fd,str,1024)) > 0) {
           write(1,str,length);
         }
