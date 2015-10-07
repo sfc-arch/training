76fa4839a85d19123ab360709dc3a4028f6528b7
makoto26 <makomori26@gmail.com>
2015-10-07 17:11:56 +0900

    Third version

--- cat.c
+++ cat.c
@@ -9,26 +9,31 @@ int main(int argc, const char *argv[]) {
   int fd;
   char *str[1024];
 
-  if (argc < 1) {
-    write(2, "An error occured.\n", 18);
-  } else if (argc == 1) {
-    while ((input = read(0, str, 1024)) > 0) {
-      write(1, str, input);
-    }
-  } else {
-    for (int i = 1; i < argc; i++) {
-      fd = open(argv[i],O_RDONLY);
+  switch (argc) {
+    case 0:
+            write(2, "An error occured.\n", 18);
+            return -1;
+    case 1:
+            while ((input = read(0, str, 1024)) > 0) {
+              write(1, str, input);
+            }
+            return -1;
+    default:
+            for (int i = 1; i < argc; i++) {
+              fd = open(argv[i],O_RDONLY);
 
-      if (fd<0) {
-        write(2, "An error occured.\n", 18);
-      } else {
-        while ((length = read(fd,str,1024)) > 0) {
-          write(1,str,length);
-        }
-      }
-      close(fd);
-    }
+              if (fd<0) {
+                write(2, "An error occured.\n", 18);
+                return -1;
+              }
+
+              if (fd >= 0 ) {
+                while ((length = read(fd,str,1024)) > 0) {
+                  write(1,str,length);
+                }
+              }
+              close(fd);
+              return 0;
+            }
   }
-
-  return 0;
 }
