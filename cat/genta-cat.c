d1e4d049548f1141b02f611d8995a8cb3bbfc239
Genta Mochizawa <g.mochizawa@gmail.com>
2015-10-30 16:18:03 +0900

    added error handling

--- cat/genta-cat.c
+++ cat/genta-cat.c
@@ -1,19 +1,33 @@
 #include <unistd.h>
 #include <fcntl.h>
+#include <errno.h>
+#include <string.h>
 
-int main(int argc, const char *argv[])
-{
-    char buf[1024]; int i, fd;
+int perror(int errnum){
+	write(2, strerror(errnum), 256);
+	write(1, "\n", 1);
+	return -1;
+}
 
-    if (argc == 1 || (argc == 2 && *argv[1] == '-')) {
-        while ((write(1, buf, read(0, buf, 1024))));
-    } else {
-        for (i = 1; i < argc; i++) {
-                if ((fd = open(argv[i], 0)) > 0)
-                        while ((write(1, buf, read(fd, buf, 1024)))) ;
-                else
-                        write(1, "No such file or directory\n", 26);
-            }
-    }
-    return 0;
+int main(int argc, const char *argv[]) {
+	char buf[1024]; int i, fd;
+
+	if (argc == 1 || (argc == 2 && *argv[1] == '-')) {
+		if (read(0, buf, 1024) < 0)
+            return perror(errno);
+        else
+            while (write(1, buf, read(0, buf, 1024)));
+	} else {
+		for (i = 1; i < argc; i++) {
+			if ((fd = open(argv[i], 0)) < 0) {
+				return perror(errno);
+			} else if(read(fd, buf, 1024) < 0) {
+				return perror(errno);
+			} else {
+				fd = open(argv[i], 0);
+				while (write(1, buf, read(fd, buf, 1024)));
+			}
+		}
+	}
+	return 0;
 }
