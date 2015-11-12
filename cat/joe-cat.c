4f154e4b7bd110aa70e89e964af5a45a38ff63b2
Akira Shoji <s13436as@sfc.keio.ac.jp>
2015-11-12 22:34:13 +0900

    add new file

---
+++ cat/joe-cat.c
@@ -0,0 +1,63 @@
+#include<unistd.h>/*open STDOUT_FILEなどのマクロ定義 */
+#include<sys/types.h>
+#include<sys/uio.h>
+#include<fcntl.h>/*read */ /*write*/ /*close*/
+#include<sys/stat.h>/*open*/
+
+
+#define BUFFER_SIZE 1024
+#define ERNOTE "error occered\n"
+#define ERNUM 15
+
+
+int main(int argc, char *argv[])
+{
+    int fd;
+    ssize_t cc;
+    char buf[BUFFER_SIZE];
+    char notation[] = "終了させる場合は、control + Dをおしてください。\n";
+
+
+    if( argc == 1 )
+    {
+        write( STDOUT_FILENO, notation, sizeof(notation));
+        while( (cc = read (STDIN_FILENO, buf, sizeof(buf) ) ) > 0)
+            {
+                if ( write( STDOUT_FILENO, buf, cc) == -1)
+                {
+                    write( STDOUT_FILENO, ERNOTE, ERNUM);
+                    return 1;
+                }
+            
+            }
+        return 0;
+    }
+
+    int i;
+    for(i = 1; i < argc; i++)
+    {
+        if(( fd = open( argv[i], O_RDONLY)) == -1)
+        {
+            write(STDOUT_FILENO, ERNOTE, ERNUM);
+            return 1;
+        }
+
+        while( ( cc = read(fd, buf, sizeof(buf) ) ) > 0)
+        {
+            if( (write( STDOUT_FILENO, buf, cc) ) == -1)
+            {
+                write(STDOUT_FILENO, ERNOTE, ERNUM);
+                return 1;
+            }
+        }
+
+        if( close(fd) == -1)
+        {
+            write( STDOUT_FILENO, ERNOTE, ERNUM);
+            return 1;
+        }
+
+    }
+    return 0;
+}
+
