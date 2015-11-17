7e8f1d6b6fac20e8acb2cbc84fd94f7ddc936bc1
george310 <s13436as@sfc.keio.ac.jp>
2015-11-17 21:16:23 +0900

    Update joe-cat.c

--- cat/joe-cat.c
+++ cat/joe-cat.c
@@ -9,7 +9,7 @@
 int openerror(void);
 int writeerror(void);
 int readerror(void);
-int closeerror(void); //プロとタイプ宣言
+int closeerror(void);
 
 int main(int argc, char *argv[])
 {
@@ -31,14 +31,12 @@ int main(int argc, char *argv[])
             }
         return 0;
     }
-
-    for(i = 1; i < argc; i++)
+    for( i = 1; i < argc; i++)
     {
-        if(( fd = open( argv[i], O_RDONLY)) == -1)
+        if( ( fd = open( argv[i], O_RDONLY) ) == -1)
         {
             openerror();
         }
-
         while( ( cc = read(fd, buf, sizeof(buf) ) ) > 0)
         {
             if( (write( STDOUT_FILENO, buf, cc) ) == -1)
@@ -46,7 +44,6 @@ int main(int argc, char *argv[])
                 writeerror();
             }
         }
-
         if( close(fd) == -1)
         {
             closeerror();
@@ -59,26 +56,25 @@ int openerror(void)
 {
     char open_error[] = "Open error\n";
     write( STDOUT_FILENO, open_error, sizeof(open_error) );
-    return 1;
+    _exit(1);
 }
-
 int writeerror(void)
 {
     char write_error[]  = "Write error\n";
     write( STDOUT_FILENO, write_error, sizeof(write_error) );
-    return 1;
+    _exit(1);
 }
 
 int readerror(void)
 {
     char read_error[] = "Read error\n";
     write( STDOUT_FILENO, read_error, sizeof(read_error) );
-    return 1;
+    _exit(1);
 }
 
 int closeerror(void)
 {
      char close_error[] = "Close error\n";
      write( STDOUT_FILENO, close_error, sizeof(close_error) );
-     return 1;
+    _exit(1);
 }
