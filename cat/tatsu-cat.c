15d7826a8f8ca3dcbf9ae69c3c678a49b5bbfadf
dooooooooinggggg <dooooooooinggggg@gmail.com>
2018-10-12 13:56:13 +0900

    Update tatsu-cat.c

--- cat/tatsu-cat.c
+++ cat/tatsu-cat.c
@@ -1,28 +1,17 @@
 #include <fcntl.h>
+#include <unistd.h>
 
 int main(int argc, char *argv[])
 {
     if (argc < 2)
-    {
         return 1;
-    }
-
-    int *fd;
-
+    int fd;
     if ((fd = open(argv[1], O_RDONLY)) < 0)
-    {
-        // fprintf(stderr, "cannot open\n");
-        exit(1);
-    }
-
+        _exit(1);
     int buflen;
     char buf[1024];
-
-    // while ((buflen = read(fd, buf, 1024)) > 0)
-    // {
-    buflen = read(fd, buf, 1024);
-    write(1, buf, buflen);
-    // }
+    while ((buflen = read(fd, buf, 1024)) > 0)
+        write(1, buf, buflen);
     close(fd);
     return 0;
 }
