1401471c34a97b6da931552c482d5f4f81213afb
dooooooooinggggg <dooooooooinggggg@gmail.com>
2018-10-12 13:58:28 +0900

    Update tatsu-cat.c

--- cat/tatsu-cat.c
+++ cat/tatsu-cat.c
@@ -3,13 +3,12 @@
 
 int main(int argc, char *argv[])
 {
+    int fd, buflen;
+    char buf[1024];
     if (argc < 2)
         return 1;
-    int fd;
     if ((fd = open(argv[1], O_RDONLY)) < 0)
         _exit(1);
-    int buflen;
-    char buf[1024];
     while ((buflen = read(fd, buf, 1024)) > 0)
         write(1, buf, buflen);
     close(fd);
