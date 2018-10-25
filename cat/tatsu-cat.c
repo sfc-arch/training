f7f0f9a1aa2216b16bd42b57f1fb45fdaafbca38
dooooooooinggggg <dooooooooinggggg@gmail.com>
2018-10-25 19:52:56 +0900

    fix

--- cat/tatsu-cat.c
+++ cat/tatsu-cat.c
@@ -1,20 +1,20 @@
 #include <fcntl.h>
 #include <unistd.h>
-
+#define N 1024
 int main(int argc, char *argv[])
 {
     int fd, buflen;
-    char buf[1024];
+    char buf[N];
     if (argc < 2)
         _exit(1);
-    for (int i = 1; i <= argc; i++)
+    for (int i = 1; i < argc; i++)
     {
         if ((fd = open(argv[i], O_RDONLY)) < 0)
         {
             close(fd);
             _exit(1);
         }
-        while ((buflen = read(fd, buf, 1024)) > 0)
+        while ((buflen = read(fd, buf, N)) > 0)
             write(1, buf, buflen);
         close(fd);
     }
