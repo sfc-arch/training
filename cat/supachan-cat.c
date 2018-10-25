3020da437798c0b69598108e2c3480f728ce1776
supachan <supachan@sfc.wide.ad.jp>
2018-10-25 18:46:36 +0900

    supachanのcat課題:1回目の修正

--- cat/supachan-cat.c
+++ cat/supachan-cat.c
@@ -2,6 +2,7 @@
 #include <fcntl.h>
 #include <string.h>
 #include <errno.h>
+#include <stdio.h>
 
 const char crlf[2] = "\r\n";
 const char msg[7] = "supacat";
@@ -10,22 +11,22 @@ void err(char* fname, int preverr);
 int errstate = 0;
 
 int main(int argc, char* argv[]) {
-	short int isopen;
-	short int isread;
+	int fdescript;
+	ssize_t readbytes;
 	char buff[256];
+	ssize_t writebytes;
 	for (int i = 1; i < argc; i++) {
-		isopen = open(argv[i], O_RDONLY);	
-		int fnamelen = strlen(argv[i]);
-		if (isopen == -1) {
-			int errornum = errno;
-			err(argv[i],errornum);
+		fdescript = open(argv[i], O_RDONLY);
+		if (fdescript == -1) {
+			err(argv[i],errno);
 		} else {
 			while(1) {
-				isread = read(isopen,buff,256);
-				if (isread > 0) {
-					write(1,buff,isread);
-				} else if (isread == 0) {
-					close(isopen);
+				readbytes = read(fdescript,buff,256);
+				if (readbytes > 0) {
+					writebytes = write(1,buff,readbytes);
+					if (writebytes == -1) err(NULL,errno);
+				} else if (readbytes == 0) {
+					close(fdescript);
 					break;
 				} else {
 					int errornum = errno;
@@ -34,16 +35,19 @@ int main(int argc, char* argv[]) {
 				}
 			}
 		}
-		write(1,crlf,2);
+		writebytes = write(1,crlf,2);
+		if (writebytes == -1) err(NULL,errno);
 	}
 	return errstate;
 }
 
 void err(char* fname, int preverr) {
-	write(1,msg,7);
-	write(1,colon,2);
-	write(1,fname,strlen(fname));
-	write(1,colon,2);
-	write(1,strerror(preverr),strlen(strerror(preverr)));
+	write(2,msg,7);
+	write(2,colon,2);
+	if(fname != NULL) {
+		write(2,fname,strlen(fname));
+		write(2,colon,2);
+	}
+	write(2,strerror(preverr),strlen(strerror(preverr)));
 	errstate = 1;
 }
