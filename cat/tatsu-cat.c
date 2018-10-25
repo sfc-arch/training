3eb766e3cef8712c3083cad61f7aeeb1df5a3961
dooooooooinggggg <dooooooooinggggg@gmail.com>
2018-10-25 19:42:29 +0900

    2ファイル以上に対応

--- cat/tatsu-cat.c
+++ cat/tatsu-cat.c
@@ -6,11 +6,17 @@ int main(int argc, char *argv[])
     int fd, buflen;
     char buf[1024];
     if (argc < 2)
-        return 1;
-    if ((fd = open(argv[1], O_RDONLY)) < 0)
         _exit(1);
-    while ((buflen = read(fd, buf, 1024)) > 0)
-        write(1, buf, buflen);
-    close(fd);
+    for (int i = 1; i <= argc; i++)
+    {
+        if ((fd = open(argv[i], O_RDONLY)) < 0)
+        {
+            close(fd);
+            _exit(1);
+        }
+        while ((buflen = read(fd, buf, 1024)) > 0)
+            write(1, buf, buflen);
+        close(fd);
+    }
     return 0;
 }
