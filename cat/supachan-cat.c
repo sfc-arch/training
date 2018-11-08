36a05f90e81592898177ee9ca70ed106acedce44
supachan <supachan@sfc.wide.ad.jp>
2018-11-08 18:52:19 +0900

    supachanのcat課題:3回目の修正

--- cat/supachan-cat.c
+++ cat/supachan-cat.c
@@ -7,35 +7,19 @@ const char crlf[2] = "\r\n";
 const char msg[7] = "supacat";
 const char colon[2] = ": ";
 void err(char* fname, int preverr);
+void dorw(char* fname, int fdescript);
 int errstate = 0;
+char buff[256];
 
 int main(int argc, char* argv[]) {
 	int fdescript;
-	ssize_t readbytes;
-	char buff[256];
-	ssize_t writebytes;
 	for (int i = 1; i < argc; i++) {
 		fdescript = open(argv[i], O_RDONLY);
 		if (fdescript == -1) {
 			err(argv[i],errno);
 			continue;
 		}
-		while(1) {
-			readbytes = read(fdescript,buff,256);
-			if (readbytes > 0 && readbytes <= 256) {
-				writebytes = write(1,buff,readbytes);
-				if (writebytes == -1) err(NULL,errno);
-			} else if (readbytes == 0) {
-				close(fdescript);
-				writebytes = write(1,crlf,2);
-				if (writebytes == -1) err(NULL,errno);
-				break;
-			} else {
-				int errornum = errno;
-				err(argv[i],errornum);
-				break;
-			}
-		}
+		dorw(argv[i],fdescript);
 	}
 	return errstate;
 }
@@ -51,3 +35,25 @@ void err(char* fname, int preverr) {
 	write(1,crlf,2);
 	errstate = 1;
 }
+
+void dorw(char* fname, int fdescript) {
+	ssize_t readbytes;
+	ssize_t writebytes;
+	while(1) {
+		readbytes = read(fdescript,buff,256);
+		if (readbytes > 0 && readbytes <= 256) {
+			writebytes = write(1,buff,readbytes);
+			if (writebytes == -1) err(NULL,errno);
+		} else if (readbytes == 0) {
+			close(fdescript);
+			writebytes = write(1,crlf,2);
+			if (writebytes == -1) err(NULL,errno);
+			break;
+		} else {
+			int errornum = errno;
+			err(fname,errornum);
+			break;
+		}
+	}
+}
+
