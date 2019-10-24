2b991bb40853d0dfc539c200e5626366a75b2f35
dooooooooinggggg <dooooooooinggggg@gmail.com>
2019-10-24 19:12:04 +0900

    エラーメッセージの出力

--- cat/tatsu-cat/tatsu-cat.c
+++ cat/tatsu-cat/tatsu-cat.c
@@ -1,6 +1,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 #define N 1024
+
 int main(int argc, char *argv[])
 {
     int fd, buflen;
@@ -11,11 +12,13 @@ int main(int argc, char *argv[])
     {
         if ((fd = open(argv[i], O_RDONLY)) < 0)
         {
+            write(1, "No such file\n\n", 32); // 標準出力
             close(fd);
             continue;
         }
+
         while ((buflen = read(fd, buf, N)) > 0)
-            write(1, buf, buflen);
+            write(1, buf, buflen); // 標準出力
         close(fd);
     }
     return 0;
